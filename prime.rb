1d0c441605a7707a8b495c36d36829638c865f85
clinton <clinton.l.anton@gmail.com>
2021-01-20 20:49:56 -0500

    commit tree a69019093158f6748fa143d93f69322f18eacb25

--- prime.rb
+++ prime.rb
@@ -5,23 +5,23 @@ require 'bigdecimal/math'
 puts Benchmark.measure { BigMath.PI(10_000) }
 
 def prime?(x)
-  if (x<=1)
-    return false;
-  elsif(x<=3)
-  return true;
-  elsif (x%2 == 0)
-    return false;
+    if (x<=1)
+      return false;
+    elsif(x<=3)
+    return true;
+    elsif (x%2 == 0)
+      return false;
+    end
+  
+  i = 5;
+  
+   while i*i <= x
+     if (x % i == 0 || x % (i+2) == 0)
+       return false;
+     end
+       i = i +6
   end
-
-i = 5;
-
- while i*i <= x
-   if (x % i == 0 || x % (i+2) == 0)
-     return false;
-   end
-     i = i +6
-end
-  return true;
-end
+    return true;
+  end
 
   
